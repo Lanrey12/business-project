b69b245e13c630278459e8f61766006792f7eda3
Lanrey12 <alexolajide197@gmail.com>
2023-12-30 22:18:10 +0000

    add redux-persist to store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from '../redux/user/userSlice'
+import { persistReducer, persistStore } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({
+    user: userReducer
+})
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store)
+// The persist reducer allows the state in the store to be stored in the localstorage, 
+// So the state doesn't change when a user refreshes it only updates the dom 
+//so basically it works like handling sessions 
