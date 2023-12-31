dd68e19b396e15e203b1009959e97c2c99dd1385
Lanrey12 <alexolajide197@gmail.com>
2023-12-31 16:18:56 +0000

    make profile route private and diplay profile img

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,20 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import SignIn from "./pages/authentication/SignIn";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn/>} />
+        <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useSelector } from "react-redux";
+import { Outlet, Navigate } from 'react-router-dom';
+
+export default function PrivateRoute() {
+    const { currentUser } = useSelector((state) => state.user);
+// using outlet as a component allows the rendering of child components when the parent component is used in the Routes 
+  return (
+    currentUser ? <Outlet/> : <Navigate to='/signin'/>
+  )
+}
