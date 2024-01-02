6495ffab10de2c63e7831986090eac75dac1e787
Lanrey12 <alexolajide197@gmail.com>
2024-01-02 21:49:08 +0000

    create update user function in client

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useRef, useState, useEffect } from "react";
 import {
   getDownloadURL,
@@ -7,18 +7,46 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
+import {
+  initialUpdateUser,
+  updateUserSuccess,
+  updateUserfailure,
+} from "../redux/user/userSlice";
 
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePercentage, setFilePercentage] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const dispatch = useDispatch();
 
-  console.log(formData);
-  console.log(filePercentage);
-  console.log(fileUploadError);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(initialUpdateUser());
+      const response = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      if (data.success === false) {
+        dispatch(updateUserfailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+    } catch (error) {
+      dispatch(updateUserfailure(error.message));
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
 
   useEffect(() => {
     if (file) {
@@ -53,7 +81,7 @@ export default function Profile() {
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type="file"
@@ -69,7 +97,10 @@ export default function Profile() {
         />
         <p className="text-sm self-center">
           {fileUploadError ? (
-            <span className="text-red-700"> Image upload unsuccessful / image must be less than 2 mb </span>
+            <span className="text-red-700">
+              {" "}
+              Image upload unsuccessful / image must be less than 2 mb{" "}
+            </span>
           ) : filePercentage > 0 && filePercentage < 100 ? (
             <span className="text-slate-700">
               {`Uploading ${filePercentage}`}
@@ -85,12 +116,16 @@ export default function Profile() {
           placeholder="username"
           className="border p-3 rounded-lg"
           id="username"
+          defaultValue={currentUser.username}
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           className="border p-3 rounded-lg"
           id="email"
+          defaultValue={currentUser.email}
+          onChange={handleChange}
         />
         <input
           type="password"
@@ -98,14 +133,15 @@ export default function Profile() {
           className="border p-3 rounded-lg"
           id="password"
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Update
+        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          {loading ? "loading..." : "Update" }
         </button>
       </form>
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer">Delete account</span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
+      <p className='text-red-700 mt-5'>{error ? error : ''}</p>
     </div>
   );
 }
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,30 +1,49 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    currentUser : null,
-    error: null,
-    loading: false
-  }  
+  currentUser: null,
+  error: null,
+  loading: false,
+};
 
-  export const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {  
-        initialSignIn : (state) => {
-         state.loading = true
-      },
-      signInSuccess : (state, action) => {
-        state.currentUser = action.payload;
-        state.loading = false;
-        state.error = null;
-     },
-     signInfailure : (state, action) => {
-        state.error = action.payload;
-        state.loading = false;
-     },
-    }
-  })
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    initialSignIn: (state) => {
+      state.loading = true;
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    signInfailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+    initialUpdateUser: (state) => {
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateUserfailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+  },
+});
 
-  export const { initialSignIn, signInSuccess, signInfailure} = userSlice.actions;
+export const {
+  initialSignIn,
+  signInSuccess,
+  signInfailure,
+  initialUpdateUser,
+  updateUserSuccess,
+  updateUserfailure,
+} = userSlice.actions;
 
-  export default userSlice.reducer
+export default userSlice.reducer;
