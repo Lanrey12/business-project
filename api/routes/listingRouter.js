f7655a2927ab79d5b86f37c90c91008c12cd1894
Lanrey12 <alexolajide197@gmail.com>
2024-01-12 20:24:06 +0000

    code clean-up

--- api/routes/listingRouter.js
+++ api/routes/listingRouter.js
@@ -5,7 +5,6 @@ import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
-// router.post("/update/:id", verifyToken, updateUser);
-// router.delete("/delete/:id", verifyToken, deleteUser);
+
 
 export default router;
