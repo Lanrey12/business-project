8b550807717b91aa580b64f0cdb40517412af820
Lanrey12 <alexolajide197@gmail.com>
2024-01-13 00:37:58 +0000

    complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -141,6 +141,24 @@ export default function Profile() {
       setShowListingError(true);
     }
   };
+
+  const handleDeleteListing = async (listingId) => {
+    try {
+      const responseData = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await responseData.json()
+      if (data.success === false) {
+        console.log(data.message)
+        return;
+      }
+
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId))
+    }catch (error){
+      console.log(error.message)
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Profile</h1>
@@ -245,7 +263,7 @@ export default function Profile() {
             </Link>
 
             <div className="flex flex-col item-center">
-              <button className="text-red-700 uppercase">
+              <button type="button" onClick={() => handleDeleteListing(listing._id)} className="text-red-700 uppercase">
                 Delete
               </button>
               <button className="text-green-700 uppercase">
