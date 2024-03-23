4be3261d42c3c99b87da068d2adc15a6e081f204
Lanrey12 <alexolajide197@gmail.com>
2024-03-23 21:18:09 +0000

    fix app.jx code cleanup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import SignUp from "./pages/Authentication/SignUp";
+import SignUp from "./pages/authentication/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
