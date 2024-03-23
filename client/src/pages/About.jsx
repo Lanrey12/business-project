47430e7206437408403a5a006efebda5034f3763
Lanrey12 <alexolajide197@gmail.com>
2024-03-23 20:58:28 +0000

    Complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About Cars Gallery</h1>
+      <p className="mb-4 text-slate-700">
+        Mazda can be summed up in a single word: bold. Not only do its cars
+        generally look like nothing else on the market, but it’s also invested
+        heavily in innovative engine technology. So, while many car makers have
+        moved to smaller turbocharged engines, Mazda instead offers larger
+        motors that are still efficient. For example, the entry-level petrol
+        option in the Mazda 3 is a 2.0-litre unit – far larger than what you’ll
+        find in most rivals – but it remains very clean. Highlights of Mazda’s
+        line-up include the MX-5 sports car, which has won our Convertible of
+        the Year Award multiple times, and the CX-5 large SUV, which was named
+        as our inaugural best car for dog owners.
+      </p>
+    </div>
+  );
 }
